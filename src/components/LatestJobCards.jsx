60f9264e6d7a4ffba8fe4c9f4f4851359bc49c75
Bevara Saranya <127865395+SaiSaranyabevara@users.noreply.github.com>
2025-06-02 14:40:14 +0530

    Update LatestJobCards.jsx

--- src/components/LatestJobCards.jsx
+++ src/components/LatestJobCards.jsx
@@ -24,7 +24,7 @@ const LatestJobCards = ({job}) => {
             </div>
             <div>
                 <h1 className='font-medium text-lg my-2'>{job?.title}</h1>
-                <p  className='text-sm text-gray-600'>{job?.description}</p>
+{/*                 <p  className='text-sm text-gray-600'>{job?.description}</p> */}
             </div>
             <div className='flex items-center gap-2 mt-4'>
                 <Badge className='text-blue-700 font-bold ' variant={"ghost"}>{job?.position} Positions</Badge>
