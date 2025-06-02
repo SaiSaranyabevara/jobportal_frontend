5ed1ceb6463072f2e1634d41cf9a6befcd26199d
Bevara Saranya <127865395+SaiSaranyabevara@users.noreply.github.com>
2025-06-02 20:55:42 +0530

    Update constant.js

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,7 +1,18 @@
-export const USER_API_END_POINT = 'http://localhost:8000/api/v1/user';
+// const  API_BASE_URL=jobportal-backend-gn6516sxy-saranya-bevaras-projects.vercel.app || 'http://localhost:8000/api/v1';
 
-export const JOB_API_END_POINT = 'http://localhost:8000/api/v1/job';
 
-export const APPLICATION_API_END_POINT = 'http://localhost:8000/api/v1/application';
+// export const USER_API_END_POINT = '${API_BASE_URL}/user';
 
-export const  COMPANY_API_END_POINT='http://localhost:8000/api/v1/company';
+// export const JOB_API_END_POINT = '${API_BASE_URL}/job';
+
+// export const APPLICATION_API_END_POINT = '${API_BASE_URL}/application';
+
+// export const  COMPANY_API_END_POINT='${API_BASE_URL}/company';
+
+
+const API_BASE_URL = 'https://jobportal-backend-gn6516sxy-saranya-bevaras-projects.vercel.app/api/v1' || 'http://localhost:8000/api/v1';
+
+export const USER_API_END_POINT = `${API_BASE_URL}/user`;
+export const JOB_API_END_POINT = `${API_BASE_URL}/job`;
+export const APPLICATION_API_END_POINT = `${API_BASE_URL}/application`;
+export const COMPANY_API_END_POINT = `${API_BASE_URL}/company`;
