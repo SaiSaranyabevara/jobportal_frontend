e1ca8045a1a4adc252da5dec615f437eb78ba60c
Bevara Saranya <127865395+SaiSaranyabevara@users.noreply.github.com>
2025-06-02 15:22:00 +0530

    Update HeroSection.jsx

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -22,7 +22,8 @@ const navigate = useNavigate();
         <div className='flex flex-col gap-5 my-10'>
         <span className='mx-auto px-4 py-2 rounded-full bg-gray-100 text-[#F83002] fornt-medium'> No.1 Job Hunt Website </span>
          <h1 className='text-5xl font-bold'>Search ,Apply & <br/> Get Your <span className='text-[#6A38C2]'>Dream Jobs</span></h1>
-         <p>Discover your dream tech job with ease — explore verified openings in Full Stack, Data Analytics, Design, and more.
+         <p>Discover your dream tech job with ease — explore verified openings in Full Stack, Data Analytics, Design, and more. 
+         </br>
 Connect with top companies, apply instantly, and take the next step in your career journey.</p>
        <div className='flex w-[40%] shadow-lg border border-gray-200 pl-3 rounded-full items-center gap-4 mx-auto'>
         <input type="text"
