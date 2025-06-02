273202ef3d457b9b020f33b29553cf1215b10cd3
Bevara Saranya <127865395+SaiSaranyabevara@users.noreply.github.com>
2025-06-02 21:48:32 +0530

    Update constant.js

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -9,8 +9,7 @@
 
 // export const  COMPANY_API_END_POINT='${API_BASE_URL}/company';
 
-
-const API_BASE_URL = 'https://jobportal-backend-gn6516sxy-saranya-bevaras-projects.vercel.app/api/v1' || 'http://localhost:8000/api/v1';
+const API_BASE_URL = 'https://jobportal-backend-nine.vercel.app/api/v1';
 
 export const USER_API_END_POINT = `${API_BASE_URL}/user`;
 export const JOB_API_END_POINT = `${API_BASE_URL}/job`;
